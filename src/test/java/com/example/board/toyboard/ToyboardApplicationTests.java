498c5c8323a24f39dcda56fa274fbfccf92686b3
HwangJaeHwan <zxcv0069@naver.com>
2022-03-31 15:40:02 +0900

    Start Spring
    start

---
+++ src/test/java/com/example/board/toyboard/ToyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.board.toyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
